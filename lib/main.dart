e4c6b3dd3ae61956b2109e9aff770a0028309aa9
Tom Malin <webdevz88@gmail.com>
2022-05-12 08:11:56 -0700

    refactored duplicate code into a function

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,16 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
-  static var _random = new Random();
-  int leftDiceNumber = _random.nextInt(6) + 1;
-  int rightDiceNumber = _random.nextInt(6) + 1;
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -37,9 +44,7 @@ class _DicePageState extends State<DicePage> {
                 child: Image.asset('images/dice$leftDiceNumber.png'),
               ),
               onPressed: () {
-                setState(() {
-                  leftDiceNumber = _random.nextInt(6) + 1;
-                });
+                changeDiceFace();
               },
             ),
           ),
@@ -50,9 +55,7 @@ class _DicePageState extends State<DicePage> {
                 child: Image.asset('images/dice$rightDiceNumber.png'),
               ),
               onPressed: () {
-                setState(() {
-                  rightDiceNumber = _random.nextInt(6) + 1;
-                });
+                changeDiceFace();
               },
             ),
           ),
